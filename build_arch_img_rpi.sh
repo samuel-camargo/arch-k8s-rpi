9aeb52fec71da3cbeb6e778f9ee0ba20ffe09b9f
Samuel Alessandro Camargo <samuel.camargo@allshares.com>
2026-04-08 14:59:25 +0100

    The "USB-Fixed" RPi 5 Patch Script

--- build_arch_img_rpi.sh
+++ build_arch_img_rpi.sh
@@ -11,7 +11,7 @@ RPI5_IMG="rpi5_master.img"
 MIRROR_CORE="http://fl.us.mirror.archlinuxarm.org/aarch64/core"
 MIRROR_ALARM="http://fl.us.mirror.archlinuxarm.org/aarch64/alarm"
 
-# Colors for logging
+# Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
@@ -27,7 +27,7 @@ cd "$WORKSPACE"
 
 log "Step 1: Cloning Base Image for RPi 5"
 if [ ! -f "$BASE_IMG" ]; then
-    error_exit "Base image $BASE_IMG not found! Please run build_arch_img.sh first."
+    error_exit "Base image $BASE_IMG not found! Ensure build_arch_img.sh (10GB version) ran first."
 fi
 
 cp "$BASE_IMG" "$RPI5_IMG"
@@ -46,10 +46,8 @@ docker run --rm --privileged \
 
     cd /work
     
-    echo 'Fixing loop device nodes...'
-    for i in {0..15}; do
-        if [ ! -b /dev/loop\$i ]; then mknod /dev/loop\$i b 7 \$i || true; fi
-    done
+    echo 'Fixing loop nodes...'
+    for i in {0..15}; do [ ! -b /dev/loop\$i ] && mknod /dev/loop\$i b 7 \$i || true; done
 
     echo 'Fetching mirror indexes...'
     CORE_LIST=\$(curl -sL $MIRROR_CORE/)
@@ -78,9 +76,9 @@ docker run --rm --privileged \
     mkdir -p /mnt/root/boot
     mount \"/dev/mapper/\$BOOT_MAPPER\" /mnt/root/boot
 
-    echo 'CLEANUP: Removing generic kernel modules to free space...'
+    echo 'CLEANUP: Removing generic modules...'
     rm -rf /mnt/root/usr/lib/modules/*
-    rm -f /mnt/root/boot/Image
+    rm -rf /mnt/root/boot/*
 
     echo 'Injecting RPi 5 Kernel and Firmware...'
     PKGS=(\"\$KERNEL_PKG\" \"\$BOOT_PKG\" \"\$FIRM_PKG\")
@@ -89,23 +87,53 @@ docker run --rm --privileged \
         tar -xpf \"\$pkg\" -C /mnt/root --exclude='.PKGINFO' --exclude='.MTREE' --exclude='.INSTALL'
     done
 
-    echo 'Enabling Headless Access (SSH/Root)...'
+    echo 'SURGERY: Fixing Boot partition directory structure...'
+    # Arch packages put files in /boot/. Since we mounted P1 to /boot, 
+    # the files end up in /boot/boot/. We must move them to the top level.
+    if [ -d /mnt/root/boot/boot ]; then
+        mv /mnt/root/boot/boot/* /mnt/root/boot/
+        rmdir /mnt/root/boot/boot
+    fi
+
+    echo 'Configuring config.txt for RPi 5 USB support...'
+    # Basic Pi 5 configuration
+    cat <<EOF > /mnt/root/boot/config.txt
+# Raspberry Pi 5 Arch Linux ARM Config
+arm_64bit=1
+enable_uart=1
+uart_2ndstage=1
+
+# Use the RPi kernel we injected
+kernel=Image
+
+# Required for RPi 5 USB (RP1 chip)
+device_tree=bcm2712-rpi-5-b.dtb
+overlay_prefix=overlays/
+
+# Enable common features
+dtparam=audio=on
+dtoverlay=vc4-kms-v3d
+max_framebuffers=2
+EOF
+
+    echo 'Configuring cmdline.txt...'
+    # rootwait is vital for SD cards
+    echo 'root=/dev/mmcblk0p2 rw rootwait console=serial0,115200 console=tty1 selinux=0 smsc95xx.turbo_mode=N' > /mnt/root/boot/cmdline.txt
+
+    echo 'Setting up Headless Access...'
     ln -sf /usr/lib/systemd/system/sshd.service /mnt/root/etc/systemd/system/multi-user.target.wants/sshd.service
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /mnt/root/etc/ssh/sshd_config
-    
     mkdir -p /mnt/root/root/.ssh && chmod 700 /mnt/root/root/.ssh
     if [ -f /work/id_rsa.pub ]; then cp /work/id_rsa.pub /mnt/root/root/.ssh/authorized_keys
     elif [ -f /work/id_ed25519.pub ]; then cp /work/id_ed25519.pub /mnt/root/root/.ssh/authorized_keys
     fi
     [ -f /mnt/root/root/.ssh/authorized_keys ] && chmod 600 /mnt/root/root/.ssh/authorized_keys
 
-    echo 'Finalizing boot files...'
+    echo 'Finalizing fstab...'
     cat <<EOF > /mnt/root/etc/fstab
 /dev/mmcblk0p1  /boot   vfat    defaults        0       0
 /dev/mmcblk0p2  /       ext4    defaults,noatime  0       1
 EOF
-
-    echo 'root=/dev/mmcblk0p2 rw rootwait console=serial0,115200 console=tty1 selinux=0 smsc95xx.turbo_mode=N' > /mnt/root/boot/cmdline.txt
 
     sync
     umount -R /mnt/root
